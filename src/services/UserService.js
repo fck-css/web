99ba92d18425a918bb13ba0f330d98581c612600
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-05-03 00:00:46 +0200

    add saveSnippet function to UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,3 +3,4 @@ import createHttp from "./BaseService";
 const http = createHttp({ useAccessToken: true })
 
 export const getCurrentUser = () => http.get('/users/me')
+export const saveSnippet = (data) => http.post('/users/snippet', data);
